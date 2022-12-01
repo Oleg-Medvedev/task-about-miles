009b12a174106cd983867286ee3c3124e12832bc
Oleg <olegmedvedev7@mail.ru>
2022-12-01 18:32:13 +0300

    Add src/

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int cost = 13560;
+        int price_Mile = 20;
+        int amount_Received_Miles = cost / price_Mile;
+        System.out.println(amount_Received_Miles);
+    }
+}
