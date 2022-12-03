fb1d8e8a56246d5396fe44a3e2b164ec6a599cf2
Oleg <olegmedvedev7@mail.ru>
2022-12-03 22:23:58 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         int cost = 13560;
-        int price_Mile = 20;
-        int amount_Received_Miles = cost / price_Mile;
-        System.out.println(amount_Received_Miles);
+        int priceMile = 20;
+        int amountReceivedMiles = cost / priceMile;
+        System.out.println(amountReceivedMiles);
     }
 }
